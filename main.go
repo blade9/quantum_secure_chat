15146276d6ae93cb82aa26479896943fcc5ebe4d
Siddarth Selvakumar <59669816+blade9@users.noreply.github.com>
2025-07-15 16:28:07 -0400

    tested simple connection to web server

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "PQC Chat Server is up and running!")
+		fmt.Println("Received a ping request")
+	})
+
+	port := "8080"
+	fmt.Printf("Server listening on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 	fmt.Println("Hello world")
+
 }
